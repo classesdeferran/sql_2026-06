771e1c13de0dd731d27aeb85e22e71dfc30d69ef
Ferran <ferran.docencia@gmail.com>
2026-06-16 17:56:16 +0200

    2026-06-16

--- tienda.sql
+++ tienda.sql
@@ -25,3 +25,41 @@ INSERT INTO productos(nombre_producto, nombre_proveedor, tel_proveedor,
 email_proveedor, ciudad_proveedor, precio_compra, precio_venta, referencia_producto) VALUES
 ("Micra", "AUTOS SA", "123456789", "info@autossa.com", "Lleida", 4000, 6000, "M1"),
 ("Raval", "AUTOS SA", "123456789", "info@autossa.com", "Lleida", 6000, 8000, "R1");
+
+SELECT * FROM productos;
+SELECT DISTINCT nombre_proveedor, tel_proveedor, email_proveedor, ciudad_proveedor FROM productos;
+
+# Subdividir la tabla productos en:
+CREATE TABLE proveedores(
+	id_proveedor INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    nombre_proveedor VARCHAR(30) NOT NULL,
+	tel_proveedor VARCHAR(9),
+	email_proveedor VARCHAR(40),
+    id_ciudad INT
+);
+
+CREATE TABLE ciudades(
+	id_ciudad INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    nombre_ciudad VARCHAR(30)
+);
+
+# introducir el nombre de la ciudad
+INSERT INTO ciudades(nombre_ciudad) VALUES("Lleida");
+SELECT * FROM ciudades;
+
+# modificar la tabla proveedores para añadir el nombre de la ciudad
+ALTER TABLE proveedores
+ADD COLUMN nombre_ciudad VARCHAR(30);
+
+SELECT * FROM proveedores;
+
+INSERT INTO proveedores(nombre_proveedor, tel_proveedor, email_proveedor, nombre_ciudad)
+SELECT DISTINCT nombre_proveedor, tel_proveedor, email_proveedor, ciudad_proveedor FROM productos;
+
+SELECT c.id_ciudad
+FROM ciudades c
+JOIN proveedores p
+ON c.nombre_ciudad = p.nombre_ciudad;
+
+
+
