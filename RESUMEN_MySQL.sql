4026005d20ce60be70673e792f8a700a2987731c
Ferran <ferran.docencia@gmail.com>
2026-06-19 17:11:19 +0200

    2026-06-19

--- RESUMEN_MySQL.sql
+++ RESUMEN_MySQL.sql
@@ -288,7 +288,7 @@ FROM consultas
 GROUP BY id_usuario;
  
 
-# Queremos saber qué usuarios tienen el coste promedio por encima de la media
+# Queremos saber qué usuarios tienen el coste promedio por encima de la media de todos los tratamientos
 SELECT id_usuario, AVG(tratamiento) as promedio_personal
 FROM consultas
 GROUP BY id_usuario
@@ -302,6 +302,207 @@ FROM consultas)
 GROUP BY id_usuario;
 */
 
+# ¿Cuál/es usuarios (nombre y apellido) han realizado más consultas?
+SELECT concat(u.nombre_usuario, " ", u.apellido_usuario) as usuario, COUNT(c.id_usuario) as consultas
+FROM usuarios u
+NATURAL JOIN consultas c
+GROUP BY c.id_usuario
+ORDER BY consultas DESC
+LIMIT 1;
+
+# ¿Cuál/es usuarios (nombre y apellido) han realizado menos consultas?
+# Solución mala, no incluye necesariamente todos los registros
+SELECT concat(u.nombre_usuario, " ", u.apellido_usuario) as usuario, COUNT(c.id_usuario) as consultas
+FROM usuarios u
+NATURAL JOIN consultas c
+GROUP BY c.id_usuario
+ORDER BY consultas ASC
+LIMIT 1;
+
+SELECT 
+    CONCAT(u.nombre_usuario,
+            ' ',
+            u.apellido_usuario) AS usuario,
+    COUNT(c.id_usuario) AS consultas
+FROM
+    usuarios u
+        NATURAL JOIN
+    consultas c
+GROUP BY c.id_usuario
+HAVING consultas = (SELECT 
+        COUNT(c.id_usuario) AS consultas
+    FROM
+        usuarios u
+            NATURAL JOIN
+        consultas c
+    GROUP BY c.id_usuario
+    ORDER BY consultas DESC
+    LIMIT 1);
+
+SELECT CONCAT(u.nombre_usuario,
+            ' ',
+            u.apellido_usuario) AS usuario,
+            COUNT(c.id_usuario) as consultas_usuarios
+FROM usuarios u
+NATURAL JOIN consultas c
+GROUP BY c.id_usuario
+HAVING consultas_usuarios > 1
+ORDER BY consultas_usuarios ASC
+LIMIT 1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* REVISAR
+USE clinica;
+EXPLAIN SELECT id_usuario, AVG(tratamiento) as promedio_personal
+FROM consultas
+GROUP BY id_usuario
+HAVING promedio_personal >(SELECT AVG(tratamiento) FROM consultas);
+
+EXPLAIN ANALYZE SELECT id_usuario, AVG(tratamiento) as promedio_personal
+FROM consultas
+GROUP BY id_usuario
+HAVING promedio_personal >(SELECT AVG(tratamiento) FROM consultas);
+
+*/
+
+/* ================================================
+	FUNCIONES DEL SISTEMA
+*/
+/* IFNULL(campo a evaluar, que mostrar si la respuesta es NULL) */
+
+# Qué usuarios sí han programado ninguna consulta todavía
+SELECT DISTINCT concat(u.nombre_usuario,
+            ' ',
+            u.apellido_usuario) AS usuario            
+FROM usuarios u
+NATURAL JOIN consultas c;
+
+# Qué usuarios no han programado ninguna consulta todavía
+SELECT concat(u.nombre_usuario,
+            ' ',
+            u.apellido_usuario) AS usuario,
+            IFNULL(c.id_consulta, 0) as consultas_programadas
+FROM usuarios u
+LEFT JOIN consultas c USING(id_usuario)
+WHERE c.id_consulta IS NULL;
+
+/* COALESCE(col_1, col_2, ...) */
+ALTER TABLE usuarios
+ADD COLUMN tel_movil VARCHAR(9),
+ADD COLUMN tel_fijo VARCHAR(9);
+
+UPDATE usuarios
+SET tel_movil = "123456789"
+WHERE id_usuario = 1;
+
+UPDATE usuarios
+SET tel_movil = "123456789"
+WHERE id_usuario = 3;
+
+UPDATE usuarios
+SET tel_fijo = "987654321"
+WHERE id_usuario = 11;
+
+UPDATE usuarios
+SET tel_fijo = "987654321"
+WHERE id_usuario = 10;
+
+SELECT concat(u.nombre_usuario,
+            ' ',
+            u.apellido_usuario) AS usuario,
+            COALESCE(tel_movil, tel_fijo, "No tenemos datos de contacto") as contacto
+FROM usuarios u;
+
+/*  IF(condicion, is_verdadero, si_falso) */
+
+# Podemos ofrecer un descuento a los usuarios que han nacido antes del año 2000
+# Queremos que aparezca si tienen o no opción a ese descuento
+
+SELECT concat(u.nombre_usuario,
+            ' ',
+            u.apellido_usuario) AS usuario,
+			IF(u.fecha_nacimiento < "2000-01-01", "Sí", "No") as opcion_descuento
+FROM usuarios u;
+
+/* DATEDIFF(fecha_1, fecha_2) -> devuelve la diferencia en días */
+# Queremos saber cuántos días han pasado desde que se pide una consulta hasta que se realiza
+SELECT id_consulta, DATEDIFF(fecha_concertada, fecha_peticion) as dias
+FROM consultas
+WHERE DATEDIFF(fecha_concertada, fecha_peticion) > 5;
+
+/* CURDATE() */
+SELECT CURDATE();
+/* NOW() */
+SELECT NOW();
+
+# Cuántos días faltan para las consultas pendientes
+SELECT id_consulta, DATEDIFF(fecha_concertada, CURDATE()) as dias_que_faltan
+FROM consultas
+WHERE DATEDIFF(fecha_concertada, CURDATE()) > 0;
+
+/* Esto provoca un error
+SELECT id_consulta, 
+IF(DATEDIFF(fecha_concertada, CURDATE()) > 0, DATEDIFF(fecha_concertada, CURDATE()), "consulta ya realizada") as dias_que_faltan
+FROM consultas
+WHERE dias_que_faltan > 1;
+*/
+
+/* RAND() -> genera números aleatorios entre 0 y 1 (pero el 1 no está incluido) */
+SELECT RAND();
+
+# números aleatorios entre 1 y 10
+SELECT FLOOR(RAND() * 10) + 1;
+
+/* SUBSTRING */
+SELECT substring("AEIOU", 2, 1);
+
+ALTER TABLE usuarios
+ADD COLUMN codigo_usuario VARCHAR(9);
+
+# Hay que actualizar los códigos de usuario con valores aleatorios
+# según este patrón: 4 letras - 4 números. Por ejemplo: ABCD-1234
 
 
 
